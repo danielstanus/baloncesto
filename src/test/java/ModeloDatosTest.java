8231433b5c784d337b2dec98edf40558bbdf47db
danielstanus <daniel.stanus@edu.uah.es>
2024-01-09 23:32:59 +0100

    Añadido Poner votos a 0 y la prueba funccional de actualizarJugador() v1

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -9,14 +9,42 @@ public class ModeloDatosTest {
     @Test 
     public void testExisteJugador() { 
         System.out.println("Prueba de existeJugador"); 
-        String nombre = "Pepito"; 
+        String nombre = "Rudy"; 
 
         ModeloDatos modeloDatos = new ModeloDatos(); 
         modeloDatos.abrirConexion();
 
-        boolean expResult = false; 
+        boolean expResult = true; 
         boolean result = modeloDatos.existeJugador(nombre); 
-        assertEquals(expResult, result); 
+        assertEquals(expResult, result,"El jugador no existe y deberia de existir"); 
     } 
+
+
+    
+    @Test 
+    public void testActualizarJugador() { 
+        System.out.println("Iniciando prueba de actualizarJugador");
+    
+        ModeloDatos modeloDatos = new ModeloDatos(); 
+        modeloDatos.abrirConexion();
+    
+        Map<String, Integer> baseDeDatosPrueba = new HashMap<>();
+    
+        // Datos de prueba
+        String nombreJugador = "Rudy";
+        baseDeDatosPrueba.put(nombreJugador, 0); 
+    
+        modeloDatos.actualizarJugador(nombreJugador);
+    
+        // Actualizar el valor del jugador en baseDeDatosPrueba después de llamar a actualizarJugador
+        Integer votosActualizados = baseDeDatosPrueba.get(nombreJugador) + 1;
+        baseDeDatosPrueba.put(nombreJugador, votosActualizados);
+    
+        // Verificar que los votos del jugador se han incrementado en 1
+        assertEquals(votosActualizados, baseDeDatosPrueba.get(nombreJugador), "Los votos del jugador no se incrementaron correctamente");
+    
+        System.out.println("Prueba de actualizarJugador completada con éxito");
+    }
+    
 
 }
