8c79ad6f829ecb11e97f8e8ae036a880b012f2fb
danielstanus <daniel.stanus@edu.uah.es>
2024-01-09 23:18:15 +0100

    Añadido Poner votos a 0 y la prueba funccional de actualizarJugador() v1

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -19,28 +19,4 @@ public class ModeloDatosTest {
         assertEquals(expResult, result); 
     } 
 
-
-  
-    public void testActualizarJugador() { 
-        System.out.println("Iniciando prueba de actualizarJugador");
-
-        ModeloDatos modeloDatos = new ModeloDatos(); 
-        modeloDatos.abrirConexion();
-
-        Map<String, Integer> baseDeDatosPrueba = new HashMap<>();
-
-        // Datos de prueba
-        String nombreJugador = "Rudy";
-        baseDeDatosPrueba.put(nombreJugador, 0); 
-
-        modeloDatos.actualizarJugador(nombreJugador);
-
-        // Verificar que los votos del jugador se han incrementado en 1
-        assertEquals(Integer.valueOf(1), baseDeDatosPrueba.get(nombreJugador), "Los votos del jugador no se incrementaron correctamente");
-
-        System.out.println("Prueba de actualizarJugador completada con éxito");
-
-
-    } 
-
 }
