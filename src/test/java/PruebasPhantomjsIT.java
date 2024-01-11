7565dda9bbb08babc194654796c2e78963bf35d7
danielstanus <daniel.stanus@edu.uah.es>
2024-01-12 00:35:23 +0100

    Añadido Ver votos y PF-A

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -2,6 +2,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,9 +17,12 @@ public class PruebasPhantomjsIT {
     private static WebDriver driver = null;
     private String baseUrl = "http://localhost:8080/Baloncesto";
 
+    @Before
+    public void setUp() throws Exception {
+        // Configura el driver PhantomJS
+        // System.setProperty("phantomjs.binary.path", "/usr/bin/phantomjs");
+        // driver = new PhantomJSDriver();
 
-    @Test
-    public void tituloIndexTest() {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setJavascriptEnabled(true);
 
@@ -30,6 +35,11 @@ public class PruebasPhantomjsIT {
                 new String[]{"--web-security=no", "--ignore-ssl-errors=yes"}
         );
         driver = new PhantomJSDriver(caps);
+
+    }
+
+    @Test
+    public void tituloIndexTest() {
         driver.navigate().to(baseUrl + "/index.html");
         assertEquals(
                 "Votacion mejor jugador liga ACB",
@@ -61,6 +71,19 @@ public class PruebasPhantomjsIT {
         for (WebElement fila : cuerpoTabla.findElements(By.tagName("tr"))) {
             WebElement celdaVotos = fila.findElements(By.tagName("td")).get(1);
             assertEquals("0", celdaVotos.getText());
+        }
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        try {
+            // Cierra el navegador al final de la prueba
+            if (driver != null) {
+                driver.quit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
